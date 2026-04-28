8679b90e926b093b20b9501bec7fd3ff7cae3961
bpdm2710-alt <bpdm2710@gmai.com>
2026-04-28 14:36:49 +0700

    resize the panel and set the panel to center

--- GamePanel.java
+++ GamePanel.java
@@ -1,14 +1,12 @@
 import javax.swing.JPanel;
 import java.awt.*;
 public class GamePanel extends JPanel {
-    public static final int WIDTH = 1280;
-    public static final int HEIGHT = 720;
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 900;
     
     public GamePanel(){
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.BLACK);
         this.setLayout(null);
     }
-
-    
 }
--- Main.java
+++ Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame("Tetris");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
         window.setVisible(true);
         window.setResizable(false);
+        window.setLocationRelativeTo(null);
     }
 }   
