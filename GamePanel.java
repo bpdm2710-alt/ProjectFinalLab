1af1d46ea881235dca8a863da6d44e8883f72355
bpdm2710-alt <bpdm2710@gmai.com>
2026-04-28 14:57:10 +0700

    add some new methods for the gameloop mechanism

--- GamePanel.java
+++ GamePanel.java
@@ -1,12 +1,37 @@
 import javax.swing.JPanel;
 import java.awt.*;
-public class GamePanel extends JPanel {
+
+public class GamePanel extends JPanel implements Runnable {
     public static final int WIDTH = 600;
     public static final int HEIGHT = 900;
+    final int FPS = 60;
+    Thread gameThread;
     
     public GamePanel(){
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.BLACK);
         this.setLayout(null);
     }
+
+    public void lauchGame (){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while (gameThread != null){
+            
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+
+
 }
