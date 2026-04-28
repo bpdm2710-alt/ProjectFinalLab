ad2e24173a9bccac0ec763c3073eb74c951b52d3
bpdm2710-alt <bpdm2710@gmai.com>
2026-04-28 14:07:08 +0700

    hahdsad

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("fuck you");
-        System.out.println("I love you");
-        System.out.println("agdagsd");
     }
 }   
